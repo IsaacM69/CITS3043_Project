620b6b8260395584e3b6642cde7ea00298d8df5c
IsaacM69 <22701047@student.uwa.edu.au>
2021-04-28 11:57:51 +0800

    Basic flask and login

---
+++ Website/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return "Sup pussys" 
+
+@app.route('/Homepage')
+def Homepage():
+    return render_template('Homepage.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    
+    error = None
+    if request.method =='POST':
+
+        if request.form['pass'] != 'bofa' or request.form['email'] != 'garth':
+            error = "Incorrect password or email"
+
+        else:
+
+            return redirect(url_for('Homepage'))
+    return render_template('Login.html', error = error)
+
+if __name__=='__main__':
+    app.run(debug=True)
