c44463a4a18004011e8a82e8109e50e7a8656a51
seventyD <dylanfellows990@gmail.com>
2021-05-05 17:09:26 +0800

    trying to get css to work in python

---
+++ app/templates/static/java/homepage_script.js
@@ -0,0 +1,45 @@
+
+function openTab(evt, tabName) {
+    var i, tab_content, tab_links;
+    tab_content = document.getElementsByClassName("tab_content");
+    for (i = 0; i < tab_content.length; i++) {
+      tab_content[i].style.display = "none";
+    }
+    tab_links = document.getElementsByClassName("tab_links");
+    for (i = 0; i < tab_links.length; i++) {
+      tab_links[i].className = tab_links[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.onkeypress = (e) =>
+  {
+    if (e.key == '1')
+    {
+        openTab(event, 'tab1')
+    }
+    if (e.key == '2')
+    {
+        openTab(event, 'tab2')
+    }
+    if (e.key == '3')
+    {
+        openTab(event, 'tab3')
+
+    }
+    if (e.key == '4')
+    {
+        openTab(event, 'tab4')
+    }
+    if (e.key == '5')
+    {
+        openTab(event, 'tab5')
+    }
+    if (e.key == '6')
+    {
+        openTab(event, 'tab6')
+
+    }
+
+  }
